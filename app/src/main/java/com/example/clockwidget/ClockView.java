0f5d7be0c425f13e1517fe5ece5d406417da1c7c
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2016-10-10 12:20:55 +0900

    コメントを追加

--- app/src/main/java/com/example/clockwidget/ClockView.java
+++ app/src/main/java/com/example/clockwidget/ClockView.java
@@ -10,8 +10,14 @@ import android.view.View;
 
 import java.util.Calendar;
 
+/**
+ * 時計を表示するためのクラス (ビュー)
+ */
 public class ClockView extends View {
 
+    /**
+     * コンストラクタ
+     */
     public ClockView(Context context) {
         super(context);
     }
@@ -24,37 +30,55 @@ public class ClockView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * 時計を描画する
+     * @param canvas 描画先
+     */
     @Override
     protected void onDraw(Canvas canvas) {
-        int x0 = getWidth() / 2;
-        int y0 = getHeight() / 2;
-        int r = Math.min(x0, y0);
+        // 座標を取得する
+        int x0 = getWidth() / 2;        // ビューの中心の x 座標
+        int y0 = getHeight() / 2;       // ビューの中心の y 座標
+        int r = Math.min(x0, y0);       // ビューの内接円の半径
 
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.YELLOW);
-        paint.setStrokeWidth(5);
+        // 円を描画する
+        Paint paint = new Paint();              // 描画するための塗料を生成する
+        paint.setStyle(Paint.Style.STROKE);     // 塗りつぶしではなく、輪郭線を描く
+        paint.setColor(Color.YELLOW);           // 色は黄色
+        paint.setStrokeWidth(5);                // 線の太さは 5 ピクセル
         RectF ovalRect = new RectF(x0 - (r - 5), y0 - (r - 5), x0 + (r - 5), y0 + (r - 5));
-        canvas.drawOval(ovalRect, paint);
+        canvas.drawOval(ovalRect, paint);       // 円を描画する
 
-        Calendar now = Calendar.getInstance();
-        int hour = now.get(Calendar.HOUR) % 12;
-        int minute = now.get(Calendar.MINUTE);
-        int second = now.get(Calendar.SECOND);
+        // 現在時刻を取得する
+        Calendar now = Calendar.getInstance();      // 現在の日付と時刻
+        int hour = now.get(Calendar.HOUR) % 12;     // 時 (12 時間制)
+        int minute = now.get(Calendar.MINUTE);      // 分
+        int second = now.get(Calendar.SECOND);      // 秒
 
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(20);
-        double h = Math.PI * 2 * (hour * 60 + minute) / (12 * 60);
-        canvas.drawLine(x0, y0, (float) (x0 + Math.sin(h) * (r * 0.5)), (float) (y0 - Math.cos(h) * (r * 0.5)), paint);
+        // 時針を描画する
+        paint.setColor(Color.RED);                      // 色は赤
+        paint.setStrokeWidth(20);                       // 線の太さは 20 ピクセル
+        //double rad1 = Math.PI * 2 * (hour * 60 + minute) / (12 * 60);
+        double rad1 = Math.PI * 2 * hour / 12;          // 時針の角度を求める
+        double x1 = x0 + Math.sin(rad1) * (r * 0.5);    // 時針の先端の x 座標
+        double y1 = y0 - Math.cos(rad1) * (r * 0.5);    // 時針の先端の y 座標
+        canvas.drawLine(x0, y0, (float) x1, (float) y1, paint);
 
-        paint.setColor(Color.GREEN);
-        paint.setStrokeWidth(10);
-        double m = Math.PI * 2 * (minute * 60 + second) / (60 * 60);
-        canvas.drawLine(x0, y0, (float) (x0 + Math.sin(m) * (r * 0.7)), (float) (y0 - Math.cos(m) * (r * 0.7)), paint);
+        // 分針を描画する
+        paint.setColor(Color.GREEN);                    // 色は緑
+        paint.setStrokeWidth(10);                       // 線の太さは 10 ピクセル
+        //double rad2 = Math.PI * 2 * (minute * 60 + second) / (60 * 60);
+        double rad2 = Math.PI * 2 * minute / 60;        // 分針の角度を求める
+        double x2 = x0 + Math.sin(rad2) * (r * 0.7);    // 分針の先端の x 座標
+        double y2 = y0 - Math.cos(rad2) * (r * 0.7);    // 分針の先端の y 座標
+        canvas.drawLine(x0, y0, (float) x2, (float) y2, paint);
 
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(5);
-        double s = Math.PI * 2 * second / 60;
-        canvas.drawLine(x0, y0, (float) (x0 + Math.sin(s) * (r * 0.9)), (float) (y0 - Math.cos(s) * (r * 0.9)), paint);
+        // 秒針を描画する
+        paint.setColor(Color.BLUE);                     // 色は青
+        paint.setStrokeWidth(5);                        // 線の太さは 5 ピクセル
+        double rad3 = Math.PI * 2 * second / 60;        // 秒針の角度を求める
+        double x3 = x0 + Math.sin(rad3) * (r * 0.9);    // 秒針の先端の x 座標
+        double y3 = y0 - Math.cos(rad3) * (r * 0.9);    // 秒針の先端の y 座標
+        canvas.drawLine(x0, y0, (float) x3, (float) y3, paint);
     }
 }
--- app/src/main/java/com/example/clockwidget/MainActivity.java
+++ app/src/main/java/com/example/clockwidget/MainActivity.java
@@ -3,40 +3,68 @@ package com.example.clockwidget;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * ClockWidget をアプリとして実行するためのクラス
+ */
 public class MainActivity extends AppCompatActivity {
 
+    // 時計のビュー
     private ClockView clockView;
 
+    // アニメーションの実行内容、null ならアニメーション終了
     private Runnable runner;
 
+    /**
+     * アプリを起動したときに呼ばれる
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // スーパークラスのメソッドを実行する
         super.onCreate(savedInstanceState);
+
+        // アプリの画面を生成する
         setContentView(R.layout.activity_main);
 
+        // 時計のビューを取得する
         clockView = (ClockView) findViewById(R.id.clockView);
     }
 
+    /**
+     * アプリが表示されるときに呼ばれる
+     */
     @Override
     protected void onResume() {
+        // スーパークラスのメソッドを実行する
         super.onResume();
 
+        // アニメーションの実行内容を設定する
         runner = new Runnable() {
             @Override
             public void run() {
                 if (runner != null) {
+                    // 時計のビューの表示内容を更新する
                     clockView.invalidate();
+
+                    // 1000 ミリ秒 (1 秒) 後に、アニメーションを実行する
                     clockView.postDelayed(runner, 1000);
                 }
             }
         };
+
+        // 1000 ミリ秒 (= 1 秒) 後に、アニメーションを実行する
         clockView.postDelayed(runner, 1000);
     }
 
+    /**
+     * アプリが表示されなくなるときに呼ばれる
+     */
     @Override
     protected void onPause() {
+        // スーパークラスのメソッドを実行する
         super.onPause();
 
+        // アニメーションを終了する
         runner = null;
     }
 }
